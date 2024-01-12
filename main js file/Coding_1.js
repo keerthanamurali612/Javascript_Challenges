553cd84521dbb512ae58645563cc567b7220a501
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-12 11:54:48 +0530

    updated

---
+++ main js file/Coding_1.js
@@ -0,0 +1,8 @@
+// Coding challenge #1: Print numbers from 1 to 10
+var output1 = '';
+
+for (let i = 1; i < 11; i += 1) {
+    output1 += i + ' ,';
+}
+
+console.log(output1.trim());
---
+++ main js file/Coding_2.js
@@ -0,0 +1,9 @@
+// Coding challenge #2: Print the odd numbers less than 100
+var output1      ='';
+for(let i=0;i<100;i++){
+    if(i%2!=0){
+        output1 += i + ' ,';
+        
+    }
+}
+console.log(output1)
