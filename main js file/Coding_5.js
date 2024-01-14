ab953fa3984fc22df59a36d3340c59a3283d9c5c
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-14 10:31:48 +0530

    3 coding today

---
+++ main js file/Coding_5.js
@@ -0,0 +1,8 @@
+// Coding challenge #5: Calculate the sum of numbers from 1 to 10
+let sum=0
+for(let i=1;i<=10;i++){
+    sum=sum+i
+   
+}
+
+console.log(`The sum of numbers from 1 to 10 = ${sum}`)
