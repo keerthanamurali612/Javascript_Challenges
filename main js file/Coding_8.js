e9bd53f6701b13a5fe0452873fabcbd9a48f4751
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-15 11:16:40 +0530

    2 coding today

---
+++ main js file/Coding_8.js
@@ -0,0 +1,9 @@
+// Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit
+
+function celsiusFahrenheit(n){
+    return n *1.8+32;
+}
+
+let r=celsiusFahrenheit(20)
+console.log(`convert from Celsius to Fahrenheit = ${r}`)
+
