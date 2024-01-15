7c8bbfacd7a459e84ef40bef5f90a95128f58f3e
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-15 10:58:38 +0530

    2 coding challenges

---
+++ main js file/Coding_6.js
@@ -0,0 +1,9 @@
+// Coding challenge #6: Calculate Factorial of 10
+let fact = 1;
+
+for(let i = 1; i <= 10; i++)
+{
+    fact = fact* i;
+}
+
+console.log(`Factorial of 10 is ${fact}`);
---
+++ main js file/Coding_7.js
@@ -0,0 +1,8 @@
+// Coding challenge #7: Calculate the sum of odd numbers greater than 10 and less than 30
+sum=0
+for (let i=10;i<=30;i++){
+    sum=sum+i
+
+}
+
+console.log(`sum of odd numbers greater than 10 and less than 30 = ${sum}`)
