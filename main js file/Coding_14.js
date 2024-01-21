9f5584be52349a978b4d0b26047212a5c00ef589
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-21 18:58:06 +0530

    1 coding

---
+++ main js file/Coding_14.js
@@ -0,0 +1,11 @@
+// Return the Sum of Two Numbers 
+// Create a function that takes two numbers as arguments and returns their sum.
+
+function twoNumber(a,b){
+    sum=a+b;
+
+    return sum
+}
+
+let sum_of_two_numbers=twoNumber(-3,-6);
+console.log(`The Sum of Two Numbers = ${sum_of_two_numbers}`);
