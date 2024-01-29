6d95d63726df25e8a592db1eb6d690766e8cb861
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-29 21:45:35 +0530

    fix bug

--- main js file/Coding_18.js
+++ main js file/Coding_18.js
@@ -4,21 +4,17 @@ const multiples = num =>{
     let count=0
  for(let i=3;i<num;i++){
 
-    if(i%3==0 && i%5==0){
+    if(i%3===0 || i%5===0){
 
         count=count+i;
-
-    
-
     }
 
 
  }
- return count
+ return count;
     
-}
+};
 
-console.log(multiples(-15)); 
 console.log(multiples(10)); 
 console.log(multiples(20)); 
 console.log(multiples(200)); 
