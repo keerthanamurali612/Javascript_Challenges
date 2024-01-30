e9d4a727d874b02c20f78205d3a856fb0b04ac3e
keerthanamurali612 <keerthanamurali099@gmail.com>
2024-01-30 18:48:08 +0530

    final coding

---
+++ main js file/Coding_19.js
@@ -0,0 +1,12 @@
+// Returning Strings
+// Write a function that given the input string name, returns the greeting statement Hello, <name> how are you doing today?
+
+const greet = name => {
+    return `"Hello, ${name} how are you doing today? This is javascript challenges"`
+  };
+  
+  console.log(greet('Rudra')); 
+  console.log(greet('Sara')); 
+
+
+
